32d46246f832fb5a9506c4cc1c9e3ff411861ed0
BlasmethiaN <dominako223@gmail.com>
2021-03-23 21:21:18 +0100

    working dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,20 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
-import { ConfigModule } from 'nestjs-dotenv'
+import { ConfigModule } from '@nestjs/config'
 import { ContributionModule } from './contribution/contribution.module'
 import { JwtParserMiddleware } from './common/middlewares/jwt-parser.middleware'
 import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [AuthModule, ContributionModule, UserModule, ConfigModule.forRoot()],
+  imports: [
+    AuthModule,
+    ContributionModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { AppModule } from './app.module'
-import { Model } from 'objection'
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
-import { knex } from 'db/knex'
-
-const isProduction = () => process.env.NODE_ENV === 'production'
 
 async function bootstrap() {
-  Model.knex(knex)
+  // Model.knex(knex)
 
-  if (isProduction()) {
-    await knex.migrate.latest()
-  }
+  // if (isProduction()) {
+  //   await knex.migrate.latest()
+  // }
 
   const app = await NestFactory.create(AppModule)
   app.use(helmet())
